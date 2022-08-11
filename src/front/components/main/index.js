ee69d2dbf65077eb43c48609212a0ef4236f65d5
obrhubr <obrhubr@gmail.com>
2022-08-11 20:17:09 +0200

    rework frontend look

--- src/front/components/main/index.js
+++ src/front/components/main/index.js
@@ -67,31 +67,34 @@ export default function Main () {
     }
 
 	return (
-		<div className="h-screen">
-            <div className="flex space-x-3 mb-4 text-sm font-medium m-8">
+		<div className="">
+            {/* <div className="flex space-x-3 mb-4 text-sm font-medium m-8">
                 <div className="flex-auto flex space-x-3">
                 <button onClick={handleClickStatus} className="w-1/2 flex items-center justify-center rounded-md bg-black text-white p-2" type="submit">Get Status</button>
                     <p className="w-1/2 flex items-center justify-center rounded-md bg-black text-white p-2 ml-3">{status}</p>
                 </div>
-            </div>
+            </div> */}
 			<div className="flex space-x-3 mb-4 text-sm font-medium m-8">
+                <div className='w-20 p-0.5 text-xl underline mr-4'>
+                    Training: 
+                </div>
                 <div className="flex-auto flex space-x-3">
-                    <button onClick={handleClickScratch} className="w-1/2 flex items-center justify-center rounded-md bg-black text-white p-2" type="submit">Train from scratch</button>
-                    <button onClick={handleClickRetrain} className="w-1/2 flex items-center justify-center rounded-md border bg-gray-300 border-gray-300 p-2" type="button">Train with newest data</button>
+                    <button onClick={handleClickScratch} className="w-1/2 flex items-center justify-center rounded-md bg-blue-400 text-white hover:bg-blue-600 hover:text-white text-lg p-2" type="submit">Train from scratch</button>
+                    <button onClick={handleClickRetrain} className="w-1/2 flex items-center justify-center rounded-md bg-yellow-500 text-white hover:bg-yellow-900 hover:text-white text-lg p-2" type="button">Train with newest data</button>
                 </div>
             </div>
             <div className="flex space-x-3 mb-4 text-sm font-medium m-8 h-1/3">
                 <textarea id="textarea-prediction" className="h-full w-1/2 flex focus:border-light-blue-500 focus:ring-1 focus:ring-light-blue-500 focus:outline-none text-sm text-black placeholder-gray-500 border border-gray-200 rounded-md p-2" type="text" aria-label="Filter projects" placeholder="Email Text" />
                 <div className="flex-auto flex w-1/2">
-                    <button onClick={handleClickPrediction} className="w-1/2 flex items-center justify-center rounded-md bg-black text-white" type="submit">Get Prediction</button>
-                    <p className="w-1/2 flex items-center justify-center rounded-md bg-black text-white p-2 ml-3">{prediction}</p>
+                    <button onClick={handleClickPrediction} className="w-1/2 flex items-center justify-center rounded-md bg-gray-300 text-gray-700 hover:bg-gray-800 hover:text-white text-lg" type="submit">Get Prediction</button>
+                    <p className="w-1/2 flex items-center justify-center rounded-md border-gray-500 border-2 text-white p-2 ml-3">{prediction}</p>
                 </div>
             </div>
             <div className="flex space-x-3 mb-4 text-sm font-medium m-8 h-1/3">
                 <textarea id="textarea-newdata" className="h-full w-1/2 flex focus:border-light-blue-500 focus:ring-1 focus:ring-light-blue-500 focus:outline-none text-sm text-black placeholder-gray-500 border border-gray-200 rounded-md p-2" type="text" aria-label="Filter projects" placeholder="Email Text" />
                 <input id="textarea-label" className="w-1/4 flex focus:border-light-blue-500 focus:ring-1 focus:ring-light-blue-500 focus:outline-none text-sm text-black placeholder-gray-500 border border-gray-200 rounded-md p-2" type="text" aria-label="Filter projects" placeholder="0 or 1, spam or not spam" />
                 <div className="flex-auto flex w-1/4">
-                    <button onClick={handleClickNewData} className="w-full flex items-center justify-center rounded-md bg-black text-white" type="submit">Add data</button>
+                    <button onClick={handleClickNewData} className="w-full flex items-center justify-center rounded-md bg-gray-300 text-gray-700 hover:bg-gray-800 hover:text-white text-lg" type="submit">Add data</button>
                 </div>
             </div>
 		</div>
--- src/front/pages/index.js
+++ src/front/pages/index.js
@@ -3,10 +3,26 @@ import { useEffect } from 'react';
 
 export default function Home() {
 
-	useEffect( () => { document.querySelector("body").classList.add("bg-yellow-400") } );
+	useEffect( () => { document.querySelector("body").classList.add("bg-white") } );
 
 	return (
 		<>
+			<nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-900 shadow-md">
+				<div class="container flex flex-wrap justify-between items-center mx-auto">
+					<a href="/" class="flex items-center">
+						<div className='mx-2 pt-1 text-blue-600'>
+							<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+								<path fillRule="evenodd" d="M14.243 5.757a6 6 0 10-.986 9.284 1 1 0 111.087 1.678A8 8 0 1118 10a3 3 0 01-4.8 2.401A4 4 0 1114 10a1 1 0 102 0c0-1.537-.586-3.07-1.757-4.243zM12 10a2 2 0 10-4 0 2 2 0 004 0z" clipRule="evenodd" />
+							</svg>
+						</div>
+						<span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Spam Classifier</span>
+					</a>
+					<button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+						<span class="sr-only">Open main menu</span>
+						<svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+					</button>
+				</div>
+			</nav>
 			<Main>
       		</Main>
 		</>
